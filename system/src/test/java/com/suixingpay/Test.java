6800177451b0425f30e9ae0d1521091b94228113
huoli-knight <158906158@qq.com>
2019-11-02 23:05:40 +0800

    student 的更新功能

--- system/src/test/java/com/suixingpay/Test.java
+++ system/src/test/java/com/suixingpay/Test.java
@@ -34,6 +34,7 @@ public class Test {
     public void alertStudent() {
         student.setEmail("123456@suixingpay.com");
         student.setSudentId("20191020");
+
         System.out.println(studentService.alertStudent(student));
     }
 }
